4691182ca3f489b52ace55aaf98242d0e5eeabc4
06ystl <50415658+06ystl@users.noreply.github.com>
2023-02-27 19:49:13 +0800

    Create buddy.js

---
+++ buddy.js
@@ -0,0 +1,11 @@
+let body = {
+  "success": true,
+  "response": {
+      "product_id": "585133",
+      "activation_id": "admin",
+      "type": "personal",
+      "expires": 1,
+      "expiry_date": 1999999999999
+  }
+}
+$done({ body: JSON.stringify(body) });
